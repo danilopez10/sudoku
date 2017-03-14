efbea23760b73d539873cc64b9687ac8c585d38b
user <user@user-PC>
2017-03-14 08:36:53 +0100

    Segunda subida del proyecto

--- src/javaapplication50/JavaApplication50.java
+++ src/javaapplication50/JavaApplication50.java
@@ -5,6 +5,8 @@
  */
 package javaapplication50;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Daniel López Avilés
@@ -15,7 +17,20 @@ public class JavaApplication50 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        Sudoku juego = new Sudoku();
+
+        System.out.println("1. Iniciar/Reiniciar sudoku → Iniciará o reiniciará el sudoku con el que se está\n"
+                + "jugando.\n"
+                + "2. Realizar movimiento → Pedirá las coordenadas de la casilla en la que se\n"
+                + "quiere poner un número y en el caso de que el número sea válido para esa\n"
+                + "casilla se guardará, también habrá que comprobar que en esa casilla no\n"
+                + "haya ningún número previamente.\n"
+                + "3. Vaciar casilla → Pedirá las coordenadas de la casilla de la que se quiere\n"
+                + "borrar un número ya existente.\n"
+                + "4. Mostrar sudoku → Mostrará el sudoku por la pantalla.\n"
+                + "5. Terminar → Termina la partida.");
+        
     }
-    
+
 }
--- src/javaapplication50/Sudoku.java
+++ src/javaapplication50/Sudoku.java
@@ -15,33 +15,54 @@ import java.util.ArrayList;
 public class Sudoku {
 
     // Variables privadas de la clase
-    private final ArrayList<ArrayList<Integer>> sudoku=new ArrayList<>();
-    
+    private ArrayList<ArrayList<Integer>> sudoku=new ArrayList<>();
+    ArrayList<Integer> linea0;
+    ArrayList<Integer> linea1;
+    ArrayList<Integer> linea2;
+    ArrayList<Integer> linea3;
+    ArrayList<Integer> linea4;
+    ArrayList<Integer> linea5;
+    ArrayList<Integer> linea6;
+    ArrayList<Integer> linea7;
+    ArrayList<Integer> linea8;
     /**
     * Constructor de la clase Sudoku
     */
     
     public Sudoku() {
-        
-        ArrayList<Integer> linea0=new ArrayList<>();
-        ArrayList<Integer> linea1=new ArrayList<>();
-        ArrayList<Integer> linea2=new ArrayList<>();
-        ArrayList<Integer> linea3=new ArrayList<>();
-        ArrayList<Integer> linea4=new ArrayList<>();
-        ArrayList<Integer> linea5=new ArrayList<>();
-        ArrayList<Integer> linea6=new ArrayList<>();
-        ArrayList<Integer> linea7=new ArrayList<>();
-        ArrayList<Integer> linea8=new ArrayList<>();
-        
+        sudoku=new ArrayList<>();
+        linea0=new ArrayList<>();
+        linea1=new ArrayList<>();
+        linea2=new ArrayList<>();
+        linea3=new ArrayList<>();
+        linea4=new ArrayList<>();
+        linea5=new ArrayList<>();
+        linea6=new ArrayList<>();
+        linea7=new ArrayList<>();
+        linea8=new ArrayList<>();
     }
     
     
     public void inicializar() {
+        for (int i = 0; i < 9; i++) {
+            linea0.add(0);
+            linea1.add(0);
+            linea2.add(0);
+            linea3.add(0);
+            linea4.add(0);
+            linea5.add(0);
+            linea6.add(0);
+            linea7.add(0);
+            linea8.add(0);
+        }
+        linea0.set(1,2);
+        
     }
 
     @Override
     public String toString() {
         String resultadoFinal = "";
+        resultadoFinal=linea0.toString()+"\n"+linea1.toString()+"\n"+linea2.toString()+"\n"+linea3.toString()+"\n"+linea4.toString()+"\n"+linea5.toString()+"\n"+linea6.toString()+"\n"+linea7.toString()+"\n"+linea8.toString();
         return resultadoFinal;
     }
 
